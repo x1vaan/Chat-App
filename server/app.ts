109ea05d69c1df8ec84776ff3f4c45524e0b4b48
x1vaan <ivanx467@gmail.com>
2023-02-20 16:40:16 -0300

    set up

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import FreeAccess from './routes/FreeAccess';
 
 const app = express();
+
+app.use('/',FreeAccess);
 
 export default app
