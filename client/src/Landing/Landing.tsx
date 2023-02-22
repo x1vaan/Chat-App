f8f98df1f37575c328baa66cd30eaf714d0b780b
x1vaan <ivanx467@gmail.com>
2023-02-21 21:47:58 -0300

    landing

--- client/src/Landing/Landing.tsx
+++ client/src/Landing/Landing.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import cssL from './Landing.module.css'
 
 export default function Landing(): JSX.Element {
     return(
-        <div>
-
+        <div className={cssL.container}>
+        <h1>Talk to your partners</h1>
         </div>
     )
 }
